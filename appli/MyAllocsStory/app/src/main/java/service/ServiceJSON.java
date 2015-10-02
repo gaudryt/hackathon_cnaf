8f8ab374b8b56c12c4f9c96421dcd2c2dfd2cb80
Théophile Gaudry <theophile.gaudry@insa-rouen.fr>
2015-10-02 21:00:42 +0200

    Ajout de la communication JSON cote appli

---
+++ appli/MyAllocsStory/app/src/main/java/service/ServiceJSON.java
@@ -0,0 +1,58 @@
+package service;
+
+import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import entites.Slide;
+
+import org.alexd.jsonrpc.*;
+
+
+/**
+ * Created by tgaudry on 02/10/15.
+ */
+public class ServiceJSON {
+
+    public List<Slide> getListeEntite(String num) throws Exception {
+        ArrayList<Slide> liste = new ArrayList<Slide>(10);
+        try {
+            JSONRPCClient client = JSONRPCClient.create("http://service/uri");
+            client.setConnectionTimeout(2000);
+            client.setSoTimeout(2000);
+            String rep = "";
+            try
+            {
+                rep = client.callString("getListeObjets",  num);
+            }
+            catch (JSONRPCException e)
+            {
+                e.printStackTrace();
+            }
+            if(rep != ""){
+                String[] format = rep.split("\n");
+                for(int i = 0; i <= 9; i++){
+                    String[] ligne = format[i].split(";");
+                    String dtStart = ligne[4];
+                    Date date = new Date();
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/aaaa");
+                    try {
+                        date = dateFormat.parse(dtStart);
+                        System.out.println(date);
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                    liste.add(new Slide(Long.parseLong(ligne[0]), ligne[1], ligne[2], ligne[3], date));
+                }
+            }
+        } catch (Exception e) {
+            throw e;
+        }
+        return liste;
+    }
+}
--- serveur/src/main/java/requesthandler/ListObjetsHandler.java
+++ serveur/src/main/java/requesthandler/ListObjetsHandler.java
@@ -36,18 +36,18 @@ public class ListObjetsHandler implements RequestHandler{
         }
     }
 	
-	private String[] genererSlides(String num){
-		String[] rep = new String[10];
-		rep[0] = "0;INF;Ceci est un test visible.;Ceci est un test caché;01/01/2010";
-		rep[1] = "1;QUE;Quelle est la couleur du cheval blanc d'Henry 4 ?;Blanc;01/02/2010";
-		rep[2] = "2;DEM;Rouen;Paris;01/03/2010";
-		rep[3] = "3;MES;Le saviez-vous ? Aujourd'hui a lieu le hackathon de la cnaf !;nill;01/04/2010";
-		rep[4] = "4;INF;Vous pouvez recevoir 1000 euros pour cette année;nill;01/05/2010";
-		rep[5] = "5;QUE;Qu'est ce qui est petit, jaune et qui fait peur ?;Un poussin avec une mitraillette;01/06/2010";
-		rep[6] = "6;DEM;Terre;Mars;01/07/2010";
-		rep[7] = "7;MES;Le saviez vous ? 1/2 des français ont droit à la CAF;Ca fait beaucoup;01/08/2010";
-		rep[8] = "8;INF;Votre fiston fête ses 3 ans;Kado;01/09/2010";
-		rep[9] = "9;QUE;Quel est l'age de la branche famille des allocations ?;60;01/10/2010";
+	private String genererSlides(String num){
+		String rep = new String();
+		rep = "0;INF;Ceci est un test visible.;Ceci est un test caché;01/01/2010\n";
+		rep = rep + "1;QUE;Quelle est la couleur du cheval blanc d'Henry 4 ?;Blanc;01/02/2010\n";
+		rep = rep + "2;DEM;Rouen;Paris;01/03/2010\n";
+		rep = rep + "3;MES;Le saviez-vous ? Aujourd'hui a lieu le hackathon de la cnaf !;nill;01/04/2010\n";
+		rep = rep + "4;INF;Vous pouvez recevoir 1000 euros pour cette année;nill;01/05/2010\n";
+		rep = rep + "5;QUE;Qu'est ce qui est petit, jaune et qui fait peur ?;Un poussin avec une mitraillette;01/06/2010\n";
+		rep = rep + "6;DEM;Terre;Mars;01/07/2010\n";
+		rep = rep + "7;MES;Le saviez vous ? 1/2 des français ont droit à la CAF;Ca fait beaucoup;01/08/2010\n";
+		rep = rep + "8;INF;Votre fiston fête ses 3 ans;Kado;01/09/2010\n";
+		rep = rep + "9;QUE;Quel est l'age de la branche famille des allocations ?;60;01/10/2010\n";
 		return rep;
 	}
 }
